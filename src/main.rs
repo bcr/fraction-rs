67fe24e3dd6d5219b4b1c3ce5662aff50efe5ab9
Blake Ramsdell <blaker@gmail.com>
2021-04-24 00:17:17 -0700

    Add mixed number support

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,38 @@
 #[derive(Debug)]
 struct Fraction {
     numerator: i32,
-    denominator: u32
+    denominator: i32
 }
 
 fn parse_fraction(fraction_string : &str) -> Fraction {
-    if fraction_string.contains("/") {
-        let mut elements = fraction_string.split("/");
-        Fraction { numerator: elements.next().unwrap().parse::<i32>().unwrap(), denominator : elements.next().unwrap().parse::<u32>().unwrap() }
-    } else {
-        let whole = fraction_string.parse::<i32>().unwrap();
-        Fraction { numerator: whole, denominator : 1}
+    let has_whole = fraction_string.contains("_");
+    let has_fraction = fraction_string.contains("/");
+    let mut whole: i32 = 0;
+    let mut numerator: i32 = 0;
+    let mut denominator: i32 = 1;
+    let mut remaining_string = fraction_string;
+
+    if !has_whole && !has_fraction {
+        whole = remaining_string.parse::<i32>().unwrap();
     }
+
+    if has_whole {
+        let mut elements = remaining_string.split("_");
+        whole = elements.next().unwrap().parse::<i32>().unwrap();
+        remaining_string = elements.next().unwrap();
+    }
+
+    if has_fraction {
+        let mut elements = remaining_string.split("/");
+        numerator = elements.next().unwrap().parse::<i32>().unwrap();
+        denominator = elements.next().unwrap().parse::<i32>().unwrap();
+    }
+
+    if whole < 0 {
+        numerator = -numerator;
+    }
+
+    Fraction { numerator: (whole * denominator) + numerator, denominator: denominator }
 }
 
 // fn parse_operator(operator_string : &str) {
@@ -52,5 +73,18 @@ mod tests {
         assert_eq!(
             parse_fraction("1/4"),
             Fraction { numerator: 1, denominator: 4 });
+        assert_eq!(
+            parse_fraction("-1/4"),
+            Fraction { numerator: -1, denominator: 4 });
+    }
+
+    #[test]
+    fn mixed() {
+        assert_eq!(
+            parse_fraction("1_1/4"),
+            Fraction { numerator: 5, denominator: 4 });
+        assert_eq!(
+            parse_fraction("-1_1/4"),
+            Fraction { numerator: -5, denominator: 4 });
     }
 }

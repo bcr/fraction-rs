359110fe103d52c958ae1bc1de501de2dd8e1df5
Blake Ramsdell <blaker@gmail.com>
2021-04-25 19:45:21 -0700

    Allow empty input to exit

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,20 @@ fn output_prompt() {
     io::stdout().flush().unwrap();
 }
 
-fn read_input() -> String {
+fn read_input() -> Result<String, std::io::Error> {
     let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
-    line
+    io::stdin().read_line(&mut line)?;
+    line = line.trim_end().to_string();
+    Ok(line)
 }
 
 fn main() {
     loop {
         output_prompt();
-        let line = read_input();
+        let line = read_input().ok().expect("Error reading user input");
+        if line.is_empty() {
+            break;
+        }
         match process_input(&line) {
             Ok(result) => println!("= {}", result),
             Err(message) => println!("Input error: {}", message)
